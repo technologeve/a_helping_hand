8f6add254d6076def8df91974022b7854993b322
rosescodes <emilyroseacc@gmail.com>
2024-11-16 18:25:11 +0000

    Initial UI

---
+++ start_page.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Welcome to: A Helping Hand")
+st.write("An EmilEve creation.")
+
+st.markdown("""
+    BLAH BLAH BLAH
+    Check it out [here] (http://localhost:8501/user_interface)"""
+            )
---
+++ user_interface.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+st.title("A Helping Hand")
+ 
+st.write("""
+Complete the exercises to earn points
+""")
+
+col1, col2 = st.columns([1,2])
+
+with col1:
+    st.markdown("### Score")
+    st.info("Display score here")
+
+    st.markdown("Next exercise")
+    st.info("Exercise here")
+
+with col2:
+    st.markdown("### You!")
+    st.info("Participant Box")
