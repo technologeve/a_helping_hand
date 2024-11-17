90b05623f7d8078dfa5778705d5ce2e641cebac9
“technologeve” <eve.nicole@icloud.com>
2024-11-17 11:42:56 +0000

    Add music and astronaut friend

--- detect_gestures.py
+++ detect_gestures.py
@@ -12,6 +12,7 @@ import threading
 # External imports
 import cv2
 import mediapipe as mp
+from playsound import playsound
 from mediapipe.tasks import python
 
 gestures = ["Pointing_Up", "Closed_Fist", "Open_Palm", 
@@ -31,16 +32,30 @@ def preload_images():
     for gesture in gestures:
         # Load image
         current_image = cv2.imread(os.path.join("images", gesture + ".png"))
-        
+
         # Resize to 300x300
         current_image = cv2.resize(current_image, (300, 300))
         images[gesture] = current_image
-        
+
         # Create a mask
         _, ret = cv2.threshold(current_image, 1, 255, cv2.THRESH_BINARY)
         masks[gesture] = ret
 
     return images, masks
+
+
+def load_astronaut():
+    """ Load astronaut image. """
+
+    astro_image = cv2.imread(os.path.join("images",  "astronaut.png"))
+
+    astro = cv2.resize(astro_image, (300, 400))
+
+    # Create a mask
+    _, astro_mask = cv2.threshold(astro, 1, 255, cv2.THRESH_BINARY)
+    print(astro.shape, astro_mask.shape)
+
+    return astro, astro_mask
 
 
 class HelpingHandGame():
@@ -61,6 +76,7 @@ class HelpingHandGame():
         self.current_gestures = []
 
         self.images, self.masks = preload_images()
+        self.astro, self.astro_mask = load_astronaut()
 
         options = GestureRecognizerOptions(
             base_options=python.BaseOptions(model_asset_path=model_path),
@@ -95,7 +111,7 @@ class HelpingHandGame():
                     mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
                     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np_array)
                     recognizer.recognize_async(mp_image, timestamp)
-                    timestamp = timestamp + 1 # should be monotonically increasing, because in LIVE_STREAM mode
+                    timestamp += 1
 
                 self.display_goal_gesture(frame)
 
@@ -121,9 +137,17 @@ class HelpingHandGame():
         # Display the goal gesture as an image
         curr_fr = cv2.bitwise_and(frame[100:400, 50:350], cv2.bitwise_not(self.masks[goal_gesture]))
         logo = cv2.bitwise_and(self.images[goal_gesture], self.masks[goal_gesture])
-        roi = cv2.bitwise_or(curr_fr,logo)
+        roi = cv2.bitwise_or(curr_fr, logo)
 
         frame[100:400, 50:350] = roi
+
+        # Display the astronaut
+        print(frame[600:1000, 1450:1750].shape, cv2.bitwise_not(self.astro_mask).shape)
+        curr_fr = cv2.bitwise_and(frame[600:1000, 1450:1750], cv2.bitwise_not(self.astro_mask))
+        logo = cv2.bitwise_and(self.astro, self.astro_mask)
+        roi = cv2.bitwise_or(curr_fr, logo)
+
+        frame[600:1000, 1450:1750] = roi
 
         # Display the user's score
         cv2.rectangle(frame, (5, 410), (200, 475), (0,0,0), -1)
@@ -165,6 +189,9 @@ def main():
                         help="If flag used, gives user randomly allocated gestures.")
 
     args = parser.parse_args()
+
+    threading.Thread(target=playsound, args=('space.mp3',), daemon=True).start()
+    # playsound('space.mp3')
 
     rec = HelpingHandGame()
     if args.random_mode:
