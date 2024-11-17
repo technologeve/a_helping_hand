1b84daa977639f062754b71621a1de773f7d160f
rosescodes <emilyroseacc@gmail.com>
2024-11-17 10:48:13 +0000

    quality_of_life_changes

--- user_interface.py
+++ user_interface.py
@@ -1,6 +1,27 @@
 from streamlit_webrtc import webrtc_streamer
 import streamlit as st
 import pandas as pd
+import base64
+
+image_path = r"C:\Users\emily\OneDrive\Desktop\a_helping_hand\space_background.jpg"
+
+def set_background_image(image_path):
+    with open(image_path, "rb") as image_file:
+        base64_image = base64.b64encode(image_file.read()).decode("utf-8")
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/jpeg;base64,{base64_image}");
+            background-size: cover;
+            background-position: center;
+        }}
+        <style>
+        """,
+        unsafe_allow_html=True
+    )
+
+set_background_image(image_path)
 
 st.title("A Helping Hand")
  
